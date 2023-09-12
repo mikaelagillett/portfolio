69983f5b2af02bd7e8119619302b4ab569fd1dcf
mikaelagillett <mikaelaihgillett@gmail.com>
2023-09-12 09:31:54 -0400

    Removed Java Script
    - had attempted an image hover but decided not to use this strategy

--- java/app.js
+++ java/app.js
@@ -1,10 +1,1 @@
-function projectImageHover() {
-  project.innerHTML = `<div class="project-description">
-            <p>description of project</p>
-            <a href="" class="btn">View Site</a>
-            <a href="" class="btn">View Source Code</a>
-          </div>`;
-}
 
-let project = document.querySelectorAll(`.project`);
-project.addEventListener("mouseover", projectImageHover);
